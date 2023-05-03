063954f24b0f1e40ee43eaed504b5a7db9f49adf
gitbhanu7 <bhanu.git2107@gmail.com>
2023-05-03 14:23:09 +0530

    initial

---
+++ src/About.js
@@ -0,0 +1,64 @@
+
+import {useState} from 'react'
+import './About.css'
+function About(){
+
+    const [arr , setArr] =  useState([])
+    const [formValues , setFormValues] =  useState({
+        name : "",
+        email  :"",
+        mobile : ""
+
+    })
+    
+    function handleInput(e){
+       console.log(e.target.name , e.target.value)
+      
+         setFormValues({...formValues ,  [e.target.name] : e.target.value})
+      
+      
+
+    }
+
+    function onAdd(){
+        setArr([...arr , formValues])
+
+        console.log(arr)
+
+    }
+
+    return(
+        <>
+        <input onChange={handleInput}  name='name'  placeholder='Enter any name'   /> 
+        <input onChange={handleInput}  name="email"  placeholder='Enter any email'   /> 
+        <input onChange={handleInput}  name="mobile"  placeholder='Enter any mobile'   /> 
+        
+        <button onClick={onAdd} >Add</button>
+
+        <table>
+            <tr>
+                <th>SR. No</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Mobile</th>
+            </tr>
+            {arr.map((el,i)=>(
+            <tr>
+                <td>{i+1}</td>
+                <td>{el.name}</td>
+                <td>{el.email}</td>
+                <td>{el.mobile}</td>
+            </tr>
+
+        ))}
+
+            
+        </table>
+
+       
+        </>
+    )
+}
+
+
+export default About
---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import About  from "./About";
+
+
+
+
+function Home(){
+
+
+    return (
+        <>
+            <h1>This is Home Comp.</h1>
+            <About/>
+        </>
+    )
+
+
+}
+
+
+export default Home;
